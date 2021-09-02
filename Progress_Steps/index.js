6cb04ab190ff2d8e8e9bbb7e880ba29444f46f74
duongHAUI <duongvipro2k1@gmail.com>
2021-09-02 07:35:59 +0700

    hoan thành

--- Progress_Steps/index.js
+++ Progress_Steps/index.js
@@ -2,30 +2,30 @@ var prev = document.querySelector("#prev");
 var next = document.querySelector("#next");
 var progress = document.querySelector("#progress");
 var circle  = document.querySelectorAll(".circle");
-var currentActive = 0;
+var currentActive = 1;
 
 
 
 next.addEventListener("click",()=>{
     currentActive++;
-    if(currentActive >=circle.length){
-        currentActive=circle.length-1;
-    }
+    // if(currentActive >circle.length){
+    //     currentActive = circle.length;
+    // }
     updateCircle();
 })
 prev.addEventListener("click",()=>{
     currentActive--;
-    if(currentActive <=0){
-        currentActive=0
-    }
+    // if(currentActive <1){
+    //     currentActive=1
+    // }
     updateCircle();
 })
 
 function updateCircle(){
-    if(currentActive===0){
+        if(currentActive===1){
         prev.disabled = true;
     }
-    else if(currentActive===circle.length-1){
+    else if(currentActive===circle.length){
         next.disabled = true;
     }
     else{
@@ -33,9 +33,13 @@ function updateCircle(){
         next.disabled = false;
     }
     circle.forEach((cir,index)=>{
-        if(currentActive===index){
-            cir.classList.add('active');
+        if(index < currentActive){
+            cir.classList.add("active");
         }
-        progress.width
+        else{
+            cir.classList.remove('active');
+        }
     })
+    var progressValue = ((currentActive-1)/(circle.length-1))*100;
+    progress.style.width = progressValue + "%";
 }
